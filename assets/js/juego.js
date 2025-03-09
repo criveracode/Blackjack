1f7308135549cbb7b6f68d51fcbbc7cf0eebf2a4
Claudio Rivera Bravo <riverabravocla@outlook.com>
2025-03-09 00:26:42 -0300

    Creación de barajas

---
+++ assets/js/juego.js
@@ -0,0 +1,30 @@
+/**
+ * 2C = TWO OF CLUBS (Tréboles)
+ * 2D = TWO OF DIAMONDS (Diamantes)
+ * 2H = TWO OF HEARTS (Corazones)
+ * 2S = TWO OF SPADES (Espada)
+ * 
+ */
+
+
+let deck = [];
+let types = ['C', 'D', 'H', 'S'];
+let specials = ['A', 'J', 'Q', 'K'];
+
+
+const createDeck = () => {
+    for (let i = 2; i <= 10; i++) {
+        for (const type of types) {
+            deck.push(i + type);
+        }
+    }
+
+    for (const type of types) {
+        for (const special of specials) {
+            deck.push(special + type);
+        }
+    }
+    console.log(deck);
+};
+
+createDeck();
